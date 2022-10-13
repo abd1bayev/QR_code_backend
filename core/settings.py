282e1439e879d3a6346c318bee5367248ed75b8b
Jasur <you@example.com>
2022-10-13 10:34:12 +0500

    all

--- core/settings.py
+++ core/settings.py
@@ -77,11 +77,9 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'USER': env('DATABASE_USER'),
-        'PASSWORD': env('DATABASE_PASS'),
-        'NAME': env('DATABASE_NAME'),
-    }
+    'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+}
 }
 
 SECRET_KEY = env('SECRET_KEY')
